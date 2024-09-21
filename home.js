e3d5866a2a1c282e8cbd826487f3655209383ec9
Miraz islam <miraz200408@gmail.com>
2024-09-22 03:40:58 +0600

    add money event handler added

---
+++ home.js
@@ -0,0 +1,26 @@
+document.getElementById('btn-add-money')
+    .addEventListener('click', function(event){
+        event.preventDefault()
+        console.log('clicked btn');
+        
+    const inputAddMoney = document.getElementById('input-add-money').value;
+    const inputPinNumber = document.getElementById('input-pin-number').value;
+    //console.log(inputAddMoney,inputPinNumber);
+    
+    if(inputPinNumber === '1234'){
+        console.log('add money success full');
+
+        const balance = document.getElementById('account-balance').innerText;
+
+        const addMoneyNumber =parseFloat(inputAddMoney);
+        const addBalance = parseFloat(balance);
+        const newBalance = addMoneyNumber + addBalance;
+
+        document.getElementById('account-balance').innerText = newBalance
+        console.log(newBalance);
+        
+    }
+    else{
+        alert('wrong pin number please try again')
+    }
+    })
--- login.js
+++ login.js
@@ -1,10 +1,18 @@
 document.getElementById('login-btn')
     .addEventListener("click", function(event){
         event.preventDefault();
-        console.log('clicked button');
+        //console.log('clicked button');
         
-        // get phone number
+        // get phone number and the pin number
     const phoneNumber = document.getElementById('phone-number').value;
-    console.log(phoneNumber);
+    const pinNumber = document.getElementById('pin-number').value;
+    //console.log(phoneNumber, pinNumber);
     
+    if(phoneNumber === '5' && pinNumber === '1234'){
+        console.log('you are login');
+        window.location.href = "home.html"
+    }
+    else{
+        alert('your phone number and pin wrong')
+    }
 })
